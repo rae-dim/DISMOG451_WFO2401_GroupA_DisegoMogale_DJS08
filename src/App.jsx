abf25f0c85203683d4c4fe1ab7ef1b0f196d7b08
Disego <disegorachelm@gmail.com>
2024-06-14 01:59:29 +0200

    add link to vandetail page

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans";
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+
+
 import "./server"
 
 function App() {
@@ -33,9 +36,11 @@ function App() {
           </nav>
         </header>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/vans" element={<Vans/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        {/* creacting routre params */}
+        <Route path="/vans/id" element={<VanDetail />}/>
       </Routes>
     </BrowserRouter>
   )
